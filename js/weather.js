9491f4b1740f36c75e337869c4a0951f281d44a5
Md Borhan Uddin Ashik <90565640+arburhan@users.noreply.github.com>
2022-03-06 12:52:23 +0600

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -2,11 +2,12 @@
 // key
 const API_KEY = `36b30d0af7a9ea4327771479b1f78d10`;
 const searchTemp=()=>{
-    const city = document.getElementById('city').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    const city = document.getElementById('city');
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayWeather(data));
+    city..value='';
 }
 const displayWeather=info=>{
     document.getElementById('w-city').innerText = `${info.name}`;
