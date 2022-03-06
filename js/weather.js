1ede8d4fda113f09348ca39bb6ad403e76cf74e4
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-03-06 12:43:55 +0600

    project complete

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,17 @@
 
+// key
 const API_KEY = `36b30d0af7a9ea4327771479b1f78d10`;
 const searchTemp=()=>{
     const city = document.getElementById('city').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => displayWeather(data));
+}
+const displayWeather=info=>{
+    document.getElementById('w-city').innerText = `${info.name}`;
+    document.getElementById('w-info').innerText = `${info.weather[0].main}`;
+    document.getElementById('w-temp').innerText=`${info.main.temp}`;
+    const url = `http://openweathermap.org/img/wn/${info.weather[0].icon}@2x.png`;
+    document.getElementById('w-image').setAttribute('src', url);
 }
